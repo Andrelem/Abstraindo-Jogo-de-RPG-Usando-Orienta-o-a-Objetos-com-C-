419f8f5a97888de06986f6882341390c45ffe264
Andre Henrique <andrehenriqueleme.lopes@gmail.com>
2024-10-08 17:55:45 -0300

    Dotnet workspace

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace dotnet_poo
+{
+    class progam
+    { 
+         static void Main(string[] args)
+         {
+            WriteLine("Olá mundo!");
+         }
+    }
+
+}
